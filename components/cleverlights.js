39af0e76605d38f9ddfc80e774ab00362d470dda
Gunnar Unger <gunnar.unger@gmail.com>
2019-04-30 09:26:42 +1000

    update

--- components/cleverlights.js
+++ components/cleverlights.js
@@ -10,14 +10,9 @@
     }
     //-------------------------------------------------------------------------------------
     var modules={
-        "car-booking-record-data":    	{url:"$H/c/car-booking-record-data.html",Table:"car-booking-record",form_module:"car-booking-record-form",router:1},
-        "car-booking-record-form":    	{url:"$H/c/car-booking-record-form.html",Table:"car-booking-record"},
-        "car-data":      	  			{url:"$H/c/car-data.html",Table:"car",form_module:"car-form",router:1},
-        "car-form":      	  			{url:"$H/c/car-form.html",Table:"car"},
-        "car-booking":   			    {url:"$H/c/car-booking.html",Table:"car","booking_form":"car-booking-record-form",router:1},
-        "car-booking-calendar": 	    {url:"$H/c/car-booking-calendar.html",Table:"car-booking-record",
-                                            "booking":"car-booking","booking_form":"car-booking-record-form",router:1},
+        "recruitment-data":    	        {url:"$H/elig-questions-data.html",Table:"cleverlight-recruitment-record",form_module:"recruitment-form",router:1},
+        "recruitment-form":    	        {url:"$H/elig-questions-form.html",Table:"ccleverlight-recruitment-record"},
     }
-    url_replace(modules,"","$H","https://modules.vmiis.com/","http://127.0.0.1:8000/vmiis/modules");
+    url_replace(modules,"","$H","https://modules.vmiis.com/","http://127.0.0.1:8000/wappsystem/cleverlights/modules");
     //-------------------------------------------------------------------------------------
 })();
