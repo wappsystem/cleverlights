859576609939220be5c0942930e78abe70749731
Zhiming Zhou <zzm.vmiis@gmail.com>
2019-08-20 16:23:55 +1000

    update

--- components/cleverlights.js
+++ components/cleverlights.js
@@ -1,5 +1,6 @@
 (function(){
     //-------------------------------------------------------------------------------------
+    /*
     var url_replace=function(modules,prefix,$H,$R,$L){
         for(m in modules){
             var $S=$R; if($vm.localhost==true) $S=$L;
@@ -8,12 +9,26 @@
             $vm.module_list[prefix+m].prefix=prefix; 
         } 
     }
+    */
     //-------------------------------------------------------------------------------------
     var modules={
-        "recruitment-data":    	        {url:"$H/elig-questions-data.html",Table:"cleverlights-recruitment-record",form_module:"recruitment-form",router:1},
-        "recruitment-form":    	        {url:"$H/elig-questions-form.html",Table:"cleverlights-recruitment-record"},
+        "recruitment-data":    	        {url:"$H/modules/elig-questions-data.html",Table:"cleverlights-recruitment-record",form_module:"recruitment-form",router:1},
+        "recruitment-form":    	        {url:"$H/modules/elig-questions-form.html",Table:"cleverlights-recruitment-record"},
     }
+    /*
     url_replace(modules,"","$H","https://wappsystem.github.io/cleverlights//modules","http://127.0.0.1:8000/wappsystem/cleverlights/modules");
     //-------------------------------------------------------------------------------------
     for(m in modules){$vm.module_list[m]=modules[m];}
+    */
+
+    for(p in modules){
+        $vm.module_list[p]=modules[p];
+        $vm.module_list[p].url=$vm.module_list[p].url.replace('$H',$vm.hosting_path);
+    }
+    if(window.location.toString().indexOf('tb=demo')!=-1){
+        for(p in $vm.module_list){
+            $vm.module_list[p].Table="demo-"+$vm.module_list[p].Table;
+        }
+    }
+
 })();
